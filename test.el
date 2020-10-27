b04648349a636d285f298cd51f73115e7725838a
Ryan Faulhaber <ryf@sent.as>
2020-10-27 19:59:16 -0400

    Started tests

--- test.el
+++ test.el
@@ -1,1 +1,12 @@
-(require "./caseconv.el")
+;; -*- lexical-binding: t; -*-
+
+(require 'caseconv "./caseconv.el")
+
+(ert-deftest split-splits-on-different-cases ()
+  (let ((result (caseconv--split-on-different-case "fooBarBaz")))
+    (should (eq t (listp result)))
+    (should (equal result (list "foo" "Bar" "Baz")))))
+
+(ert-deftest split-does-not-split-on-same-case ()
+  (let ((result (caseconv--split-on-different-case "foobarbaz")))
+    (should (string= "foobarbaz" (car result)))))
